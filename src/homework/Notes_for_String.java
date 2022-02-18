8386a6c2996e2fb053547a10bafa26fba2813023
ermalnikci <nikci.ermal@gmail.com>
2022-02-17 20:59:14 -0500

    String class

---
+++ src/homework/Notes_for_String.java
@@ -0,0 +1,23 @@
+package homework;
+
+public class Notes_for_String {
+
+
+
+    //  No.       Method   Description
+    //1  char charAt(int index) returns character value for the particular index
+    //2  int length()   returns string length
+    //3  String substring(int beginIndex)   returns substring for given begin index.
+    //4  String substring(int beginIndex, int endIndex) returns substring for given begin index and end index.
+    //5  boolean contains(CharSequence string)  returns true or false after matching the sequence of char value.
+    //6  boolean equals(String anotherString)   checks the equality of string with another given String.
+    //7   boolean equalsIgnoreCase(String another)   compares another string. It doesn't check case.
+    //8  boolean isEmpty()  checks if string is empty.
+    //9  String concat(String str)  concatenates the specified string.
+    //10 String replace(char old, char new) replaces all occurrences of the specified char value.
+    //11 String replace(CharSequence old, CharSequence new) replaces all occurrences of the specified CharSequence.
+    //12 String[] split(String regex) returns a split string matching regex. eg. words=s1.split("\\s");//splits the string based on whitespace
+    //13 String toLowerCase()   returns a string in lowercase.
+    //14 String toUpperCase()   returns a string in uppercase.
+    //15 String trim()  removes beginning and ending spaces of this string.
+}
